c6610accbc37740036120c664d374e2547c79afa
Colin Francis <francico@oregonstate.edu>
2021-12-30 14:50:33 -0500

    Added a function to push a value onto the stack. Added a function to upsize the underlying static array housed in the dynamic array struct

--- main.c
+++ main.c
@@ -19,6 +19,23 @@ struct dynamicArray {
     int *array;  // a pointer to the array
 };
 
+void printArray(struct dynamicArray *dynArrPtr) {
+    printf("(%d/%d): [", dynArrPtr->size, dynArrPtr->capacity);
+
+    if (dynArrPtr->size == 0) {
+        printf("]\n");
+        return;
+    }
+
+    for (int index = 0; index < dynArrPtr->size; index++) {
+        if (index < dynArrPtr->size - 1) {
+            printf("%d, ", *(dynArrPtr->array + index));
+        } else {
+            printf("%d]\n", *(dynArrPtr->array + index));
+        }
+    }
+}
+
 /**
  * @brief Create a new dynamic array data structure
  * 
@@ -36,9 +53,62 @@ struct dynamicArray newDynamicArray() {
     return dynArr;
 }
 
+/**
+ * @brief Transfers elements of the provided array into a new array with
+ * double the capacity
+ * 
+ * @param arrPtr The address of the dynamic array struct
+ */
+void upSizeArray(struct dynamicArray *arrPtr) {
+    // allocate memory on the heap for the new array with 2x capacity
+    int *newArray = (int*) malloc(2 * arrPtr->capacity * sizeof(int));
+
+    // transfer elements from old array to the new array
+    for (int index = 0; index < arrPtr->size; index++) {
+        newArray[index] = *(arrPtr->array + index);
+    }
+
+    // free existing array from memory and update pointer in dynamic array 
+    // struct to point to the new array
+    free(arrPtr->array);
+    arrPtr->array = newArray;
+    
+    // double array capacity
+    arrPtr->capacity = 2 * arrPtr->capacity;
+}
+
+/**
+ * @brief Pushes a new value to the top of the stack
+ * 
+ * @param value The value to push
+ * @param stackPtr The address of the dynamic array struct being used to
+ * represent a stack
+ */
+void push(int value, struct dynamicArray *stackPtr) {
+    // check if the underlying static array needs to be up sized
+    if ((float) (stackPtr->size + 1) / (float) (stackPtr->capacity) >= TABLE_RATIO) {
+        upSizeArray(stackPtr);
+    }
+
+    *(stackPtr->array + stackPtr->size) = value;  // push value to the end of the stack
+    stackPtr->size++;  // increment array size
+}
+
 int main(void) {
     struct dynamicArray stack = newDynamicArray();
     struct dynamicArray queue = newDynamicArray();
+
+    push(10, &stack);
+    printArray(&stack);
+
+    push(5, &stack);
+    printArray(&stack);
+
+    push(15, &stack);
+    printArray(&stack);
+
+    push(2, &stack);
+    printArray(&stack);
 
     return 0;
 }
