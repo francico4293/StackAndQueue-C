9693c04fe995acfebb15c54344c4c84ba3eb171d
Colin Francis <francico@oregonstate.edu>
2021-12-30 14:19:20 -0500

    Created a function to create a new dynamic array

--- main.c
+++ main.c
@@ -11,6 +11,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define TABLE_RATIO 0.75
+
+struct dynamicArray {
+    int size;  // current number of elements in the array
+    int capacity;  // current allowable number of element in the array
+    int *array;  // a pointer to the array
+};
+
+/**
+ * @brief Create a new dynamic array data structure
+ * 
+ * @return struct dynamicArray A struct representing a dynamic array
+ */
+struct dynamicArray newDynamicArray() {
+    // declare and initialize a dynamic array struct
+    struct dynamicArray dynArr = {.size=0, .capacity=5, .array=NULL};
+    // allocate memory on the heap for underlying static array
+    int *statArr = (int*) malloc(dynArr.capacity * sizeof(int));
+
+    // set array member to statArr address
+    dynArr.array = statArr;
+
+    return dynArr;
+}
+
 int main(void) {
+    struct dynamicArray stack = newDynamicArray();
+    struct dynamicArray queue = newDynamicArray();
+
     return 0;
 }
