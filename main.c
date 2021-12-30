c64e02f0b513b4c60d1721e5e6835dce4ac7dc07
Colin Francis <francico@oregonstate.edu>
2021-12-30 14:10:09 -0500

    Initial commit

---
+++ main.c
@@ -0,0 +1,16 @@
+/**
+ * @file main.c
+ * @author Colin Francis
+ * @brief An implementation of a stack and a queue in C
+ * @version 0.1
+ * @date 2021-12-30
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+    return 0;
+}
